eefd08c98571870f170af5c5460b6d31f3c982b4
Sravan Gunaganti <88744881+SravanGunaganti@users.noreply.github.com>
2022-10-11 16:53:15 +0530

    Update javascriptcleancode.js

--- javascriptcleancode.js
+++ javascriptcleancode.js
@@ -1,14 +1,14 @@
-# Javascript cleancode practice
+// Javascript cleancode practice
 
-# 1.Use meaningful and pronounceable variable names
+// 1.Use meaningful and pronounceable variable names
 
-#Bad:
+//Bad:
 
 const yyyymmdstr = new Date();
 const fname = 'Sravan';
 const lname = 'Gunaganti';
 
-#Good:
+//Good:
 
 const currentDate = new Date();
 const firstName = 'Sravan';
@@ -17,11 +17,11 @@ const lastName = 'Gunaganti';
 
 
 
-# 2.Don't add unneeded context
+// 2.Don't add unneeded context
 
-#If your class/object name tells you something, don't repeat that in your variable name.
+//If your class/object name tells you something, don't repeat that in your variable name.
 
-#Bad:
+//Bad:
 
 const Car = {
   carMake: "Honda",
@@ -33,7 +33,7 @@ function paintCar(car, color) {
   car.carColor = color;
 }
 
-#Good:
+//Good:
 
 const Car = {
   make: "Honda",
@@ -47,9 +47,9 @@ function paintCar(car, color) {
 
 
 
-# 3.Function names should say what they do.
+// 3.Function names should say what they do.
 
-#Bad:
+//Bad:
 
 const Person = {
     firstName: "Sravan",
@@ -60,7 +60,7 @@ function name(person) {
   return `${person.firstName} ${person.lastName}`
 }
 
-#Good:
+//Good:
 
 const Person = {
   firstName: "Sravan",
@@ -74,18 +74,18 @@ function getFullName(person) {
 
 
 
-# 4.Use default arguments instead of short circuiting or conditionals
+// 4.Use default arguments instead of short circuiting or conditionals
 
 /*Default arguments are often cleaner than short circuiting. Be aware that if you use them, your function will only provide default values for undefined arguments. Other "falsy" values such as '', "", false, null, 0, and NaN, will not be replaced by a default value.*/
 
-#Bad:
+//Bad:
 
 function getUserData(name) {
   const userName = name || "sravan;
   // ...
 }
 
-#Good:
+//Good:
 
 function createMicrobrewery(name = "sravan") {
   // ...
@@ -93,15 +93,15 @@ function createMicrobrewery(name = "sravan") {
 
 
 
-# 5.Remove commented code
+// 5.Remove commented code
 
-#Bad:
+//Bad:
 
 doSomething();
 // doOtherStuff();
 //doSomeMoreStuff();
 
 
-#Good:
+//Good:
 
 doSomething();
